6e4767c571649a48a30d6953f96068e54159b337
Arthur Douillard <douill_a@epita.fr>
2017-02-26 18:31:47 +0100

    fun: Improve fun-algo -> Silver.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,31 +9,34 @@ object Player extends App {
   def width  = 30
   def height = 20
 
+  sealed trait State
+  case class Empty() extends State
+  case class Occupied(id: Int) extends State
 
-  def getValAt(grid: Array[Int], x: Int, y: Int): Int = {
+  def getValAt(grid: Array[State], x: Int, y: Int): State = {
     grid(x + y * width)
   }
 
-  def setValAt(grid: Array[Int], va: Int, x: Int, y: Int): Unit =
+  def setValAt(grid: Array[State], va: State, x: Int, y: Int): Unit =
     if (x != -1 || y != -1)
       grid(x + y * width) = va
 
 
-  def initGrid(default: Int): Array[Int] = {
-    def fillLine: Array[Int] =
+  def initGrid(default: State): Array[State] = {
+    def fillLine: Array[State] =
       Array.fill(width)(default)
 
-    def fillGrid(y: Int, grid: Array[Int]): Array[Int] = {
+    def fillGrid(y: Int, grid: Array[State]): Array[State] = {
       if (y >= height)
         grid
       else
         fillGrid(y + 1, grid ++ fillLine)
     }
 
-    fillGrid(0, Array[Int]())
+    fillGrid(0, Array[State]())
   }
 
-  def getValidNeighbours(grid: Array[Int], x: Int, y: Int): Array[(Int, Int)] = {
+  def getValidNeighbours(grid: Array[State], x: Int, y: Int): Array[(Int, Int)] = {
     def possibleMoves: Array[(Int, Int)] =
       Array((0, -1), (-1, 0), (0, 1), (1, 0))
 
@@ -41,7 +44,7 @@ object Player extends App {
       x >= 0 && x < width && y >= 0 && y < height
 
     def cond(x: Int, y: Int): Boolean =
-      inGrid(x, y)  && getValAt(grid, x, y) == -2
+      inGrid(x, y)  && getValAt(grid, x, y) == Empty()
 
     possibleMoves
       .map(fact => (x + fact._1, y + fact._2))
@@ -49,38 +52,52 @@ object Player extends App {
   }
 
 
-  def computeScore(grid: Array[Int], coord: (Int, Int), me: Int): String = {
-    def endCond(grid: Array[Int]): Boolean = !grid.contains(-2)
+  def computeScore(grid: Array[State], coord: (Int, Int), me: Int): String = {
+    val movesAround: Array[(Int, Int)] =
+      Array((0, -1), (-1, 0), (0, 1), (1, 0), (0, -2), (-1, -1),
+        (-2, 0), (-1, 1), (0, 2), (1, 1), (2, 0), (1, -1))
 
-    def getScore(grid: Array[Int]): Int = grid.count(_ == me)
+    def validTile(grid: Array[State], x: Int, y: Int): Boolean =
+      x >= 0 && x < width && y >= 0 && y < height && getValAt(grid, x, y) == Empty()
 
-    def allMoves(grid: Array[Int], x: Int, y: Int, depth: Int): Array[Int] =
+    def getScore(grid: Array[State], x: Int, y: Int): Int = {
+      movesAround.map(move => (x + move._1, y + move._2))
+        .count(move => validTile(grid, move._1, move._2))
+    }
+
+    def allMoves(grid: Array[State], x: Int, y: Int, depth: Int): Array[Int] =
       Array(helper(grid.clone, x, y - 1, depth),
         helper(grid.clone, x - 1, y, depth),
         helper(grid.clone, x, y + 1, depth),
         helper(grid.clone, x + 1, y, depth))
 
-    def helper(grid: Array[Int], x: Int, y: Int, depth: Int): Int = {
-      if (x < 0 || x >= width || y < 0 || y >= height || getValAt(grid, x, y) != -2
-        || getValidNeighbours(grid, x, y).size <= 1)
+    def helper(grid: Array[State], x: Int, y: Int, depth: Int): Int = {
+      if (!validTile(grid, x, y) || getValidNeighbours(grid, x, y).length <= 1)
         -10000000
-      else if (depth >= 7)
-        getScore(grid)
+      else if (depth >= 3)
+        getScore(grid, x, y)
       else {
-        setValAt(grid, me, x, y)
+        setValAt(grid, Occupied(me), x, y)
         allMoves(grid, x, y, depth + 1).max
       }
     }
 
     val x = coord._1
     val y = coord._2
-    Array((helper(grid.clone, x, y - 1, 0), "UP"),
-      (helper(grid.clone, x - 1, y, 0), "LEFT"),
-      (helper(grid.clone, x, y + 1, 0), "DOWN"),
-      (helper(grid.clone, x + 1, y, 0), "RIGHT")).maxBy(_._1)._2
+    val moves = Array((helper(grid.clone, x, y - 1, 0), "UP", (x , y - 1)),
+      (helper(grid.clone, x - 1, y, 0), "LEFT", (x - 1, y)),
+      (helper(grid.clone, x, y + 1, 0), "DOWN", (x , y + 1)),
+      (helper(grid.clone, x + 1, y, 0), "RIGHT", (x + 1, y)))
+
+    val correctMoves = moves .filter(move => validTile(grid, move._3._1, move._3._2))
+    if (correctMoves.isEmpty)
+      "UP" // We are dead anyway
+    else
+      correctMoves.maxBy(_._1)._2
   }
 
-  val grid: Array[Int] = initGrid(-2)
+
+  val grid: Array[State] = initGrid(Empty())
   /*
    *  On the grid:
    *    - true means the tile is untouched.
@@ -101,15 +118,13 @@ object Player extends App {
       val Array(x0, y0, x1, y1) = for(i <- readLine split " ") yield i.toInt
 
       // Leave trace on the board with the id of the lightcycle.
-      setValAt(grid, i, x0, y0)
-      setValAt(grid, i, x1, y1)
+      setValAt(grid, Occupied(i), x0, y0)
+      setValAt(grid, Occupied(i), x1, y1)
 
       if (i == p)
         coord = (x1, y1)
     }
 
-    val t0 = System.currentTimeMillis()
     println(computeScore(grid, coord, p))
-    System.err.println(System.currentTimeMillis() - t0)
   }
 }
